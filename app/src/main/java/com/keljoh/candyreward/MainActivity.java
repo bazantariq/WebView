b4301cc8287052dc2034e43e370414a248d39887
Khuzaima Tariq <tariqmahmood@yandex.com>
2023-08-05 09:33:31 +0500

    1.0

---
+++ app/src/main/java/com/keljoh/candyreward/MainActivity.java
@@ -0,0 +1,164 @@
+package com.keljoh.candyreward;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import org.imaginativeworld.oopsnointernet.callbacks.ConnectionCallback;
+import org.imaginativeworld.oopsnointernet.dialogs.signal.DialogPropertiesSignal;
+import org.imaginativeworld.oopsnointernet.dialogs.signal.NoInternetDialogSignal;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener{
+    WebView webview;
+    SwipeRefreshLayout swipeLayout;
+    String currentUrl="https://ezxsohxe6.play.gamezop.com/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipescreen);
+        swipeLayout.setOnRefreshListener(this);
+
+        // No Internet Dialog: Signal
+        NoInternetDialogSignal.Builder builder = new NoInternetDialogSignal.Builder(
+                this,
+                getLifecycle()
+        );
+        DialogPropertiesSignal properties = builder.getDialogProperties();
+        properties.setConnectionCallback(new ConnectionCallback() { // Optional
+            @Override
+            public void hasActiveConnection(boolean hasActiveConnection) {
+                // ...
+            }
+        });
+        properties.setCancelable(false); // Optional
+        properties.setNoInternetConnectionTitle("No Internet"); // Optional
+        properties.setNoInternetConnectionMessage("Check your Internet connection and try again"); // Optional
+        properties.setShowInternetOnButtons(true); // Optional
+        properties.setPleaseTurnOnText("Please turn on"); // Optional
+        properties.setWifiOnButtonText("Wifi"); // Optional
+        properties.setMobileDataOnButtonText("Mobile data"); // Optional
+        properties.setOnAirplaneModeTitle("No Internet"); // Optional
+        properties.setOnAirplaneModeMessage("You have turned on the airplane mode."); // Optional
+        properties.setPleaseTurnOffText("Please turn off"); // Optional
+        properties.setAirplaneModeOffButtonText("Airplane mode"); // Optional
+        properties.setShowAirplaneModeOffButtons(true); // Optional
+        builder.build();
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        LoadWeb();
+    }
+
+    public void LoadWeb() {
+        webview = findViewById(R.id.webview786);
+        swipeLayout.setRefreshing(true);
+        webview.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webview.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+
+
+        //optional items
+        webview.getSettings().setDatabaseEnabled(true);
+        webview.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webview.getSettings().setUseWideViewPort(true);
+        webview.getSettings().setSavePassword(true);
+        webview.getSettings().setSaveFormData(true);
+        webview.getSettings().setEnableSmoothTransition(true);
+     /*   CookieManager cookieManager = CookieManager.getInstance();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            cookieManager.setAcceptThirdPartyCookies(webview,true);
+        } else
+            { cookieManager.setAcceptCookie(true);
+            }*/
+        //optional items
+
+
+        webview.loadUrl(currentUrl);
+
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Snackbar.make(view, "Connection Error", Snackbar.LENGTH_LONG)
+                        .setAction("Retry", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                LoadWeb();
+                            }
+                        }).show();
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                swipeLayout.setRefreshing(false);
+                currentUrl = url;
+                super.onPageFinished(view, url);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView wv, String url) {
+                if(url.startsWith("tel:") || url.startsWith("whatsapp:")) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    startActivity(intent);
+                    webview.goBack();
+                    return true;
+                }
+
+                if(url.startsWith("market") || url.startsWith("https://play")) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    startActivity(intent);
+                    webview.goBack();
+                    return true;
+                }
+
+                if(url.startsWith("com.facebook.katana") || url.startsWith("https://facebook")) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    startActivity(intent);
+                    // myWebView.goBack();
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+        webview.canGoBack();
+        webview.setOnKeyListener(new View.OnKeyListener() {
+
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_BACK
+                        && event.getAction() == MotionEvent.ACTION_UP
+                        && webview.canGoBack()) {
+                    webview.goBack();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+    @Override
+    public void onRefresh() {
+        LoadWeb();
+    }
+}
